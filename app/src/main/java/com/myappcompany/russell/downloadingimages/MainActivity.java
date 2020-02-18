46496779bc40c9791345256076b0d4740d8202e6
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-18 06:17:43 -1000

    ImageDownloader class created

--- app/src/main/java/com/myappcompany/russell/downloadingimages/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/downloadingimages/MainActivity.java
@@ -1,13 +1,34 @@
 package com.myappcompany.russell.downloadingimages;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    public void downloadImage(View view){
+        Log.i("Button Tapped", "ok");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            return null;
+        }
     }
 }
